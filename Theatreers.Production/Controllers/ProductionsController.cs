1c6088c032e8fc6b3b54ee00b56744efc08c1a22
Christian Reddington <christian.reddington@microsoft.com>
2018-11-18 22:54:52 +0000

    Update Theatreers.Production to use Theatrees.Shared

--- Theatreers.Production/Controllers/ProductionsController.cs
+++ Theatreers.Production/Controllers/ProductionsController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using theatreers.shared.Models;
-using theatreers.shared.Interfaces;
+using Theatreers.Shared.Models;
+using Theatreers.Shared.Interfaces;
 
 namespace Theatreers.Production.Controllers
 {
