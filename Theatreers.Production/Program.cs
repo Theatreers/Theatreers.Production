928d3e4eaa56ea5578d46a54e963284dd3539b50
Christian Reddington <christian.reddington@microsoft.com>
2018-11-27 13:15:31 +0000

    Initial commit for App Insights Integration

--- Theatreers.Production/Program.cs
+++ Theatreers.Production/Program.cs
@@ -19,6 +19,7 @@ namespace Theatreers.Production
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseApplicationInsights();
     }
 }
