ffaa43d36591865e35122ea633e28d51d33ad2be
Christian Reddington <christian.reddington@microsoft.com>
2019-01-12 11:08:25 +0000

    Include production endpoint in health path

--- Theatreers.Production/Startup.cs
+++ Theatreers.Production/Startup.cs
@@ -36,7 +36,7 @@ namespace Theatreers.Production
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseHealthChecks("/health");
+            app.UseHealthChecks("/productions/health");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
